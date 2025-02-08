72d0c00bbd483d828af0fcf5c3ff66c7a58badcf
Dwi Safitri <safitridwi533@gmail.com>
2025-02-08 21:02:58 +0800

    file upload

---
+++ story_content/user.js
@@ -0,0 +1,467 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "5hStsQs7gvP":
+        Script1();
+        break;
+      case "5WjzaZnaLpb":
+        Script2();
+        break;
+      case "6YghyuIfqsv":
+        Script3();
+        break;
+      case "6G0m8VbLzbv":
+        Script4();
+        break;
+      case "6Zh6kszAk4z":
+        Script5();
+        break;
+      case "6LCoP0Dcw2J":
+        Script6();
+        break;
+      case "6AanxjkX0cF":
+        Script7();
+        break;
+      case "6g4tqa5HiWt":
+        Script8();
+        break;
+      case "62YTg8EvWZ8":
+        Script9();
+        break;
+      case "5oxrqxsWc8D":
+        Script10();
+        break;
+      case "6BmXKPFHMqA":
+        Script11();
+        break;
+      case "6AdGDwF7TEx":
+        Script12();
+        break;
+      case "5vgDwZanva2":
+        Script13();
+        break;
+      case "5xyY0nnglZ0":
+        Script14();
+        break;
+      case "6Ktzqq8CF1B":
+        Script15();
+        break;
+      case "5tpaftHO5Cc":
+        Script16();
+        break;
+      case "6MWeIIa8fdx":
+        Script17();
+        break;
+      case "6Po9JEihMkt":
+        Script18();
+        break;
+      case "5xuHkttDzbq":
+        Script19();
+        break;
+      case "6FZUmtNlVt2":
+        Script20();
+        break;
+      case "67L6y3DQYHR":
+        Script21();
+        break;
+      case "6FztUKU0vJ1":
+        Script22();
+        break;
+      case "6gseHrhWHKt":
+        Script23();
+        break;
+      case "6OTmASoXYjL":
+        Script24();
+        break;
+      case "5qr5qHISGOE":
+        Script25();
+        break;
+      case "6nmVvNPUpGu":
+        Script26();
+        break;
+      case "5b41W6cvufo":
+        Script27();
+        break;
+      case "6DnztcYeEZw":
+        Script28();
+        break;
+      case "6JcexkjIIFB":
+        Script29();
+        break;
+      case "6LJPhFYeOHm":
+        Script30();
+        break;
+      case "6RB4oUlyTPr":
+        Script31();
+        break;
+      case "5ZuvFN5ul2U":
+        Script32();
+        break;
+      case "5X2vCXlcn7I":
+        Script33();
+        break;
+      case "6W3X85QzE43":
+        Script34();
+        break;
+      case "5l4fo7tX0Cg":
+        Script35();
+        break;
+      case "5rG7ShFvKfu":
+        Script36();
+        break;
+      case "5lEuklPwHiJ":
+        Script37();
+        break;
+      case "6N4YF6xlCWi":
+        Script38();
+        break;
+      case "6YrrAHAmZKB":
+        Script39();
+        break;
+      case "6lzsvNrJbZE":
+        Script40();
+        break;
+      case "64mUzcoGGJS":
+        Script41();
+        break;
+      case "66IsoBSVPxN":
+        Script42();
+        break;
+      case "5Yk80rkxn01":
+        Script43();
+        break;
+      case "6SEeKjLxIS9":
+        Script44();
+        break;
+  }
+}
+
+function Script1()
+{
+  //load the scripts dynamically into the head of the document
+function add_line() {
+   var line = document.createElement("audio");   
+   var head=document.getElementsByTagName('body')[0];   
+   line.type = "audio/mp3";    
+   line.src="";   
+   line.id="bgSong" ;	
+   line.autoplay = true;	
+   line.loop = true;  
+   head.appendChild(line);
+}
+
+//but we only want to add these once!
+if(document.getElementById('bgSong')==null){	
+   add_line();
+   var audio = document.getElementById('bgSong');
+   audio.volume = 0.5;
+}
+
+
+
+}
+
+function Script2()
+{
+  var audio = document.getElementById('bgSong');
+audio.src="story_content/external_files/audiosafitri.mp3";
+audio.load();
+audio.play();
+
+}
+
+function Script3()
+{
+  var audio = document.getElementById('bgSong');
+audio.pause();
+
+}
+
+function Script4()
+{
+  var audio = document.getElementById('bgSong');
+audio.play();
+
+}
+
+function Script5()
+{
+  var audio = document.getElementById('bgSong');
+audio.pause();
+
+}
+
+function Script6()
+{
+  var audio = document.getElementById('bgSong');
+audio.play();
+
+}
+
+function Script7()
+{
+  var audio = document.getElementById('bgSong');
+audio.pause();
+
+}
+
+function Script8()
+{
+  var audio = document.getElementById('bgSong');
+audio.play();
+
+}
+
+function Script9()
+{
+  var audio = document.getElementById('bgSong');
+audio.pause();
+
+}
+
+function Script10()
+{
+  var audio = document.getElementById('bgSong');
+audio.play();
+
+}
+
+function Script11()
+{
+  var audio = document.getElementById('bgSong');
+audio.pause();
+
+}
+
+function Script12()
+{
+  var audio = document.getElementById('bgSong');
+audio.play();
+
+}
+
+function Script13()
+{
+  var audio = document.getElementById('bgSong');
+audio.pause();
+
+}
+
+function Script14()
+{
+  var audio = document.getElementById('bgSong');
+audio.play();
+
+}
+
+function Script15()
+{
+  var audio = document.getElementById('bgSong');
+audio.pause();
+
+}
+
+function Script16()
+{
+  var audio = document.getElementById('bgSong');
+audio.play();
+
+}
+
+function Script17()
+{
+  var audio = document.getElementById('bgSong');
+audio.pause();
+
+}
+
+function Script18()
+{
+  var audio = document.getElementById('bgSong');
+audio.play();
+
+}
+
+function Script19()
+{
+  var audio = document.getElementById('bgSong');
+audio.pause();
+
+}
+
+function Script20()
+{
+  var audio = document.getElementById('bgSong');
+audio.play();
+
+}
+
+function Script21()
+{
+  var audio = document.getElementById('bgSong');
+audio.pause();
+
+}
+
+function Script22()
+{
+  var audio = document.getElementById('bgSong');
+audio.play();
+
+}
+
+function Script23()
+{
+  var audio = document.getElementById('bgSong');
+audio.pause();
+
+}
+
+function Script24()
+{
+  var audio = document.getElementById('bgSong');
+audio.play();
+
+}
+
+function Script25()
+{
+  var audio = document.getElementById('bgSong');
+audio.pause();
+
+}
+
+function Script26()
+{
+  var audio = document.getElementById('bgSong');
+audio.play();
+
+}
+
+function Script27()
+{
+  var audio = document.getElementById('bgSong');
+audio.pause();
+
+}
+
+function Script28()
+{
+  var audio = document.getElementById('bgSong');
+audio.play();
+
+}
+
+function Script29()
+{
+  var audio = document.getElementById('bgSong');
+audio.pause();
+
+}
+
+function Script30()
+{
+  var audio = document.getElementById('bgSong');
+audio.play();
+
+}
+
+function Script31()
+{
+  var audio = document.getElementById('bgSong');
+audio.pause();
+
+}
+
+function Script32()
+{
+  var audio = document.getElementById('bgSong');
+audio.play();
+
+}
+
+function Script33()
+{
+  var audio = document.getElementById('bgSong');
+audio.pause();
+
+}
+
+function Script34()
+{
+  var audio = document.getElementById('bgSong');
+audio.play();
+
+}
+
+function Script35()
+{
+  var audio = document.getElementById('bgSong');
+audio.pause();
+
+}
+
+function Script36()
+{
+  var audio = document.getElementById('bgSong');
+audio.play();
+
+}
+
+function Script37()
+{
+  var audio = document.getElementById('bgSong');
+audio.pause();
+
+}
+
+function Script38()
+{
+  var audio = document.getElementById('bgSong');
+audio.play();
+
+}
+
+function Script39()
+{
+  var audio = document.getElementById('bgSong');
+audio.pause();
+
+}
+
+function Script40()
+{
+  var audio = document.getElementById('bgSong');
+audio.play();
+
+}
+
+function Script41()
+{
+  var audio = document.getElementById('bgSong');
+audio.pause();
+
+}
+
+function Script42()
+{
+  var audio = document.getElementById('bgSong');
+audio.play();
+
+}
+
+function Script43()
+{
+  var audio = document.getElementById('bgSong');
+audio.pause();
+
+}
+
+function Script44()
+{
+  var audio = document.getElementById('bgSong');
+audio.play();
+
+}
+
